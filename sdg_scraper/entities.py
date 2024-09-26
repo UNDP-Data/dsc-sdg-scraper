39718e775f9d98bf77f94acc0c4995f927d46ade
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-09-26 13:07:42 +0200

    Feat(entities): add a module defining data models

--- sdg_scraper/entities.py
+++ sdg_scraper/entities.py
@@ -1,3 +1,52 @@
 """
 Entity (data) models with in-built type validation.
 """
+
+from pydantic import BaseModel, Field
+
+__all__ = ["File", "Metadata", "Publication"]
+
+
+class File(BaseModel):
+    url: str = Field(
+        description="A URL to a file.",
+        examples=[
+            "https://www.undp.org/sites/g/files/zskgke326/files/2023-10/undp-the-climate-dictionary-v3.pdf"
+        ],
+    )
+    name: str | None = Field(
+        default=None,
+        description="Name of the downloaded file, corresponding to the hash of its content.",
+        examples=["bc2b6376fc5d16f9ea18824c32fba551.pdf"],
+    )
+
+
+class Metadata(BaseModel):
+    source: str = Field(
+        description="Source URL to a publication page. Can be a direct URL or DOI redirect.",
+        examples=["https://www.undp.org/publications/climate-dictionary"],
+    )
+    title: str | None = Field(
+        description="Publication title.",
+        examples=["The Climate Dictionary"],
+    )
+    type: str | None = Field(
+        default=None,
+        description="Publication type.",
+        examples=["Guidelines, handbooks, toolkits"],
+    )
+    year: int | None = Field(
+        description="Publication year.",
+        examples=[2023],
+    )
+    labels: list[int] = Field(
+        description="SDG labels for the publication.",
+        examples=[[13, 17]],
+    )
+
+
+class Publication(Metadata):
+    files: list[File] | None = Field(
+        default=None,
+        description="Files from the publication page. One publication can have multiple linked files.",
+    )

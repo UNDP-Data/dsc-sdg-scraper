e6f3375642c1b7645e64ef6893721371054916ab
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-10-07 13:40:29 +0200

    Test: add basic tests for the CLI

---
+++ tests/test_cli.py
@@ -0,0 +1,30 @@
+"""
+A test suite for the CLI.
+"""
+
+import pytest
+from click.testing import CliRunner
+
+from sdg_scraper import __main__ as cli
+from sdg_scraper.utils import list_scrapers
+
+
+def test_list():
+    runner = CliRunner()
+    result = runner.invoke(cli.list)
+    assert result.exit_code == 0
+    assert "undp" in result.output
+
+
+@pytest.mark.parametrize(
+    "source",
+    list_scrapers(),
+)
+def test_run(source, tmp_path):
+    runner = CliRunner()
+    path = tmp_path / source
+    path.mkdir()
+    result = runner.invoke(cli.run, ["-s", source, "-p", "1", "1", "-f", str(path)])
+    assert result.exit_code == 0
+    assert len(list(path.glob("publications-*.jsonl"))) == 1, "Metadata file is missing"
+    assert len(list(path.glob("*.pdf"))) > 0, "No files have been downloaded"
